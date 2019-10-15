580cca9bc78fd06de2ae847c8c3291d25bd87222
Tom Cox <thomasxcox731@gmail.com>
2019-10-14 20:11:37 -0500

    Initial friends object array created in friend.js

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friends = {
+    "name": "Alice",
+    "photo": "",
+    "scores": [1, 2, 3, 4, 5, 1, 2 ,3, 4, 5]
+},
+{
+    "name": "Julie", 
+    "photo": "",
+    "scores": [2, 3, 4, 5, 1, 2, 3, 4, 5, 1]
+},
+{
+    "name": "Mark", 
+    "photo": "", 
+    "scores": [3, 4, 5, 1, 2, 3, 4, 5, 1, 2]
+}, 
+{
+    "name": "Gerome", 
+    "photo": "",
+    "scores": [4, 5, 1, 2, 3, 4, 5, 1, 2, 3]
+};
