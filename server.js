5342a2e32b2f9e3ab0cf9b8b540c0dac5deccafc
Tom Cox <thomasxcox731@gmail.com>
2019-10-14 21:09:17 -0500

    Server.js coded out. Should be complete for the time being.

--- server.js
+++ server.js
@@ -1,4 +1,20 @@
+//NPM Dependencies
 var express = require("express");
 var app = express ();
+
+//Linking to the PORT
 var PORT = process.env.PORT || 8080;
+
+//Express setup for data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Router
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+//Listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
 
